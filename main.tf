1d23476b52bb1c4d3e0dfebba1090fa828aec0e5
Puneeth Nanjundaswamy <puneeth.nanjundaswamy@comtravo.com>
2018-10-09 11:01:43 +0200

    Add enable flag

--- main.tf
+++ main.tf
@@ -5,26 +5,34 @@ variable "trigger" {
 variable "delay_interval" {
   description = "Sleep interval in seconds"
 }
+
 variable "command" {
   description = "Command to execute after delay"
 }
 
 variable "enable" {
-  default = 0
+  description = "0 to disable, 1 to enable"
+  default     = 0
 }
 
 resource "null_resource" "delay" {
+  count = "${var.enable}"
+
   provisioner "local-exec" {
     command = "sleep ${var.delay_interval}"
   }
+
   triggers = {
     "before" = "${var.trigger}"
   }
 }
 
 resource "null_resource" "command" {
+  count = "${var.enable}"
+
   provisioner "local-exec" {
     command = "${var.command}"
   }
+
   depends_on = ["null_resource.delay"]
 }
